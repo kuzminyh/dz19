602690621da30e572e2e0102796ddeb9eb39196a
Sergey Kuzminykh <kuzminyhsa@gmail.com>
2021-03-04 16:42:29 +0800

    правил 1 свайпер

--- js/index.js
+++ js/index.js
@@ -4,34 +4,39 @@ const choices = new Choices(element, {
    searchEnabled: false,
     itemSelectText: ''
 });
-// document.querySelector('.choices__item').addEventListener('click', function (e) {
-//   console.log(5)
-//   document.querySelector('.select-list').classList.toggle('open')
-// })
 
 // swiper
 var mySwiper1 = new Swiper('.swiper1', {
-    // Optional parameters
     slidesPerView: 3,
     slidesPerGroup: 3,
     slidesPerColumn: 2,
     spaceBetween: 50,
-    // direction: 'horizontal',
-    // loop: true,
-
-    // If we need pagination
     pagination: {
        el: '.swiper-pagination-custom',
        type: 'fraction',
     },
-
-    // Navigation arrows
     navigation: {
        nextEl: '.swiper-button-next-custom',
        prevEl: '.swiper-button-prev-custom',
     },
-
+   //  breakpoints: {
+   //    // when window width is >= 1024px
+   //    1024: {
+   //       slidesPerView: 3,
+   //       slidesPerGroup: 3,
+   //       slidesPerColumn: 2,
+   //       spaceBetween: 50,
+   //    },
+   //     768: {
+   //       slidesPerView: 2,
+   //       slidesPerGroup: 2,
+   //       slidesPerColumn: 2,
+   //       spaceBetween: 50,
+   //    },
+      
+   // }
  })
+
  var mySwiper2 = new Swiper('.swiper2', {
    // Optional parameters
    slidesPerView: 3,
@@ -73,6 +78,25 @@ var mySwiper3 = new Swiper('.swiper3', {
    },
 
 })
+// Изменение слайдера при изм размера
+window.matchMedia('(max-width: 1024px)').addListener(function(e){
+   // console.log(e)
+   // document.querySelector('mySwiper1').destroy();
+   var mySwiper1 = new Swiper('.swiper1', {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 50,
+      pagination: {
+         el: '.swiper-pagination-custom',
+         type: 'fraction',
+      },
+      navigation: {
+         nextEl: '.swiper-button-next-custom',
+         prevEl: '.swiper-button-prev-custom',
+      },
+   })
+});
 
 // Тултипы
 tippy('.projects__tooltip1', {
